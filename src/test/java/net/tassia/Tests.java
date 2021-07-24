9162e311c67af09c9234a8432b69e0fcc0050319
Tassilo <tassia710@gmail.com>
2021-07-24 14:36:00 +0200

    Add tests

---
+++ src/test/java/net/tassia/Tests.java
@@ -0,0 +1,20 @@
+package net.tassia;
+
+import java.util.Random;
+
+public class Tests {
+
+	/**
+	 * How many times a randomized test should be repeated for enough entropy to cover most edge cases.
+	 */
+	public static final int ENTROPY_ITERATIONS = 4096 * 8;
+
+	public static final Random RANDOM = new Random();
+
+	public static byte[] randomData() {
+		byte[] buffer = new byte[RANDOM.nextInt(4096)];
+		RANDOM.nextBytes(buffer);
+		return buffer;
+	}
+
+}
