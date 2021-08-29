2970bf9b37d10cf11b9a0f7aea50228edf585b9e
Tassilo <tassia710@gmail.com>
2021-08-29 22:20:23 +0200

    Add documentation

--- commons-utils/src/commonMain/kotlin/net.tassia/System.kt
+++ commons-utils/src/commonMain/kotlin/net.tassia/System.kt
@@ -1,11 +1,33 @@
 package net.tassia
 
+/**
+ * This object allows for accessing system-based functions.
+ *
+ * @since Commons 1.0
+ * @author Tassilo
+ */
 @Namespace
 expect object System {
 
+	/**
+	 * This field contains the current platform.
+	 */
 	internal val PLATFORM: Platform
 
+
+
+	/**
+	 * Returns the current time since the UNIX epoch (1st Jan 1970) in seconds.
+	 *
+	 * @return the current time in seconds
+	 */
 	fun currentTime(): Long
+
+	/**
+	 * Returns the current time since the UNIX epoch (1st Jan 1970) in milliseconds.
+	 *
+	 * @return the current time in milliseconds
+	 */
 	fun currentTimeMillis(): Long
 
 
