bde04dbe2efec2072f8fcdb997988fd29df3defa
Tassilo <tassia710@gmail.com>
2021-08-30 10:43:49 +0200

    Implement 'neq'

--- commons-assertions/src/commonMain/kotlin/net.tassia.assertions/PendingDslEquals.kt
+++ commons-assertions/src/commonMain/kotlin/net.tassia.assertions/PendingDslEquals.kt
@@ -2,14 +2,15 @@ package net.tassia.assertions
 
 import net.tassia.assertions.dsl.PendingValueBuilder
 import net.tassia.assertions.struct.EqualAssertion
+import net.tassia.assertions.struct.NotEqualAssertion
 import net.tassia.assertions.struct.SameAssertion
 
 infix fun <T : Any?> PendingValueBuilder<T>.eq(that: T): EqualAssertion<T> {
 	return EqualAssertion(that, value, name)
 }
 
-infix fun <T : Any?> PendingValueBuilder<T>.neq(that: T): EqualAssertion<T> {
-	TODO()
+infix fun <T : Any?> PendingValueBuilder<T>.neq(that: T): NotEqualAssertion<T> {
+	return NotEqualAssertion(that, value, name)
 }
 
 infix fun <T : Any?> PendingValueBuilder<T>.same(that: T): SameAssertion<T> {
